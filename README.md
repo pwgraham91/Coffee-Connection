f311b87461e78f1525c62f81e2566f031f6cf35f
Peter Graham <pwgraham91@gmail.com>
2018-03-20 15:30:42 -0500

    first commit in template mode. set up api and working on rect now

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Coffee-Connection
 App to help coworkers build connections across departments
+
+# Run stuff
+
+## Express-server
+- make sure mongo is running with `mongod`
+- cd to express-server
+- `yarn start`
+
+## React
+
---
+++ express-server/models/todo.server.model.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+var Schema = mongoose.Schema({
+  createdAt:{
+    type: Date,
+    default: Date.now
+  },
+  fullName: String,
+  todoText: String
+});
+export default mongoose.model('Todo', Schema);
