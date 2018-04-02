e45ecb3140bcd9847949380feca0ef73f1000561
Peter Graham <pwgraham91@gmail.com>
2018-04-02 18:24:34 -0500

    Implement Unittest Testing Framework For Backend Tests (#3)

---
+++ app/test/unittest_main.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+# import tests here to register them and they'll get run when this file is run
+from app.test.user import UserTestCase
+from app.test.connection import ConnectionTestCase
+
+
+if __name__ == '__main__':
+    unittest.main()
