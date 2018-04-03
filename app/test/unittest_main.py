8a01db2c157e836c18b3abd23b783461cdff3214
Peter Graham <pwgraham91@gmail.com>
2018-04-03 10:50:49 -0500

    move imports

--- app/test/unittest_main.py
+++ app/test/unittest_main.py
@@ -2,11 +2,11 @@ import unittest
 import os
 
 
-# import tests here to register them and they'll get run when this file is run
-from app.test.user import UserTestCase
-from app.test.connection import ConnectionTestCase
-
-
 if __name__ == '__main__':
     os.environ['TESTING'] = 'true'
+
+    # import tests here to register them and they'll get run when this file is run
+    from app.test.user import UserTestCase
+    from app.test.connection import ConnectionTestCase
+
     unittest.main()
