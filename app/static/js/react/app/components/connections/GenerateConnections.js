c88f46a4e67c77bdb2f884fb4a6ad817802c79d8
Peter Graham <pwgraham91@gmail.com>
2018-04-10 09:55:36 -0500

    generate connections click

--- app/static/js/react/app/components/connections/GenerateConnections.js
+++ app/static/js/react/app/components/connections/GenerateConnections.js
@@ -2,10 +2,15 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default class GenerateConnections extends React.Component {
+  onClickGenerateConnections() {
+    // todo do fetch here to generate connections
+    console.log('generator')
+  }
+
   render() {
     return (
       <div>
-        <button>Generate Connections</button>
+        <button onClick={this.onClickGenerateConnections}>Generate Connections</button>
       </div>
     )
   }
