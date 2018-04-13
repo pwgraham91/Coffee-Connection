63cb73b5d3dad179ab6760c3db47b0f3a6d2d7de
Peter Graham <pwgraham91@gmail.com>
2018-04-13 17:03:09 -0500

    add 6 and validation for num connections

--- app/test/lib/connection.py
+++ app/test/lib/connection.py
@@ -34,8 +34,9 @@ class ConnectionLibTestCase(unittest.TestCase):
                 self.assertNotIn(match, cant_match_with)
                 cant_match_with.append(match)
 
-            # each user should have a number of matches or 1 less than that number
-            self.assertIn(len(matches), [len(connections) / 2, (len(connections) / 2) + 1])
+        # assert that the user with the most matches only has one more match than the user with the lowest amount of
+        # matches
+        self.assertLessEqual(max(map(len, user_matches.values())), min(map(len, user_matches.values())) + 1)
 
     def test_make_connection_even_num_all_active(self):
         user_matches = {}
@@ -54,6 +55,36 @@ class ConnectionLibTestCase(unittest.TestCase):
         u4 = User(name='steve', email='steve@example.com')
         self.session.add(u4)
         user_matches[u4.name] = []
+
+        for i in range(len(user_matches.keys()) + 1):
+            generate_connections(self.session)
+            self.validate_connections(deepcopy(user_matches))
+
+    def test_make_connection_even_num_all_active_6(self):
+        user_matches = {}
+        u1 = User(name='john', email='john@example.com')
+        self.session.add(u1)
+        user_matches[u1.name] = []
+
+        u2 = User(name='mike', email='mike@example.com')
+        self.session.add(u2)
+        user_matches[u2.name] = []
+
+        u3 = User(name='joe', email='joe@example.com')
+        self.session.add(u3)
+        user_matches[u3.name] = []
+
+        u4 = User(name='steve', email='steve@example.com')
+        self.session.add(u4)
+        user_matches[u4.name] = []
+
+        u5 = User(name='fred', email='fred@example.com')
+        self.session.add(u5)
+        user_matches[u5.name] = []
+
+        u6 = User(name='adam', email='adam@example.com')
+        self.session.add(u6)
+        user_matches[u6.name] = []
 
         for i in range(len(user_matches.keys()) + 1):
             generate_connections(self.session)
