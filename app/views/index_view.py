3d625fddc620d2e799eae81a89a86cd7ec5c3dc9
Peter Graham <pwgraham91@gmail.com>
2018-05-05 18:06:47 -0500

    fix /index

--- app/views/index_view.py
+++ app/views/index_view.py
@@ -5,7 +5,6 @@ from app.views.handlers.auth_handler import get_google_authorization_url
 
 
 @app.route('/')
-@app.route('/index')
 @app.route('/profile/<int:profile_id>')
 def index(profile_id=None):
     # configure flask to take all 404s and render index. then in react, render a 404 if it's not found from there
