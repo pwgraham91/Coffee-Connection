dca59bc1cb6a381b77e28ac19c2a97431e34d310
Peter Graham <pwgraham91@gmail.com>
2018-06-11 17:26:04 -0500

    navigation

--- app/static/js/react/app/containers/Root.dev.js
+++ app/static/js/react/app/containers/Root.dev.js
@@ -5,7 +5,6 @@ import {Route} from 'react-router-dom';
 import {ConnectedRouter} from 'react-router-redux';
 
 import App from '../components/App';
-import DevTools from './DevTools';
 
 export default function Root({store, history}) {
     return (
@@ -14,7 +13,8 @@ export default function Root({store, history}) {
                 <ConnectedRouter history={history}>
                     <Route path="/" component={App}/>
                 </ConnectedRouter>
-                <DevTools />
+                {/* put back for redux devtools */}
+                {/*<DevTools />*/}
             </div>
         </Provider>
     );
